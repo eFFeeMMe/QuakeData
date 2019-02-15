f8db136cc3ba4595c417cb5b9ab60c63f0883f9c
Francesco Mastellone <francesco.mastellone@gmail.com>
2019-02-15 04:06:48 +0100

    still issues with static files

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
     path('ingvapi/', include('ingvapi.urls')),
 ]
