91e166366f6e469bbc76e8dd12976ee5c82d7038
Francesco Mastellone <francesco.mastellone@gmail.com>
2019-02-15 04:00:31 +0100

    still issues with static files

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'vuestuff/ingvspa/dist'),
+  os.path.join(BASE_DIR, 'vuestuff/ingvspa/dist/static'),
 ]
 
 django_heroku.settings(locals())
